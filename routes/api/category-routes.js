5b422716b4dabe9033a8e0ca819f1ef13647d105
Al <awessonn@gmail.com>
2023-05-08 13:07:07 -0700

    created update, create, and delete for category-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -15,21 +15,49 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // include its associated Products
+  try{
+    const catID = await Category.findByPk(req.params.id({ include: [{ model: Product}]}));
+    res.status(200).json(catID);
+  }
+  catch(err){
+    res.status(500).json({message: "Cannot be found.."});
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try{
+    const createCategory = await Category.create(req,body);
+    res.status(200).json(createCategory);
+  }
+  catch(err){
+    res.status(500).json({message: "Cannot be created.."});
+  }
+
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try{
+    const updateCategory = await Category.update(req.body, {where: {id: req.params.id}});
+  }
+  catch(err){
+    res.status(500).json({message: "Cannot be updated.."});
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try{
+    const deleteCategory = await Category.delete({where: {id: req.params.id}});
+  }
+  catch(err){
+    res.status(500).json({message: "Cannot be deleted.."});
+  }
+  
 });
 
 module.exports = router;
